246b2f6748fe84c032a7d92ca7197aa7ff41db47
Felipe <felipemoraespalomo@gmail.com>
2024-02-28 09:37:51 -0300

    adicionando test nos comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,28 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-});
+
+    test('Deve adicionar 2 comentarios', () => {
+        render(<Post/>)
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Primeiro comentario',
+            }
+        })
+        fireEvent.click(screen.getByTestId('comment-button'))
+
+        expect(screen.getByText('Primeiro comentario')).toBeInTheDocument()
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Segundo comentario',
+            }
+        })
+
+        fireEvent.click(screen.getByTestId('comment-button'))
+
+        expect(screen.getByText('Segundo comentario')).toBeInTheDocument()
+    });
+
+})
